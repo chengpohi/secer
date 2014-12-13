748c50f5ecb7f5a6c67d1076b462cb5275804eb2
xiachen <chengpohi@gmail.com>
2014-12-13 23:56:23 +0800

    update url checker to regex

--- src/main/scala/org/bugogre/crawler/url/Url.scala
+++ src/main/scala/org/bugogre/crawler/url/Url.scala
@@ -6,7 +6,9 @@ import org.bugogre.crawler.exception.UrlIllegalException
  * Created by xiachen on 12/13/14.
  */
 case class Url(url: String) {
-  if (!url.startsWith("http")) {
-    throw new UrlIllegalException("Url is Illegal")
+  val urlRegex = """^http.*""".r
+  url match {
+    case urlRegex() =>
+    case _ => throw new UrlIllegalException("Url is Illegal")
   }
 }
