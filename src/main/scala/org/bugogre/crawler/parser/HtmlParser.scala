8924c097d1e1e9b0b637fb244950116a631e29fd
xiachen <chengpohi@gmail.com>
2014-12-24 12:26:15 +0800

    add HtmlParser

--- src/main/scala/org/bugogre/crawler/parser/HtmlParser.scala
+++ src/main/scala/org/bugogre/crawler/parser/HtmlParser.scala
@@ -1,13 +1,20 @@
 package org.bugogre.crawler.parser
+import java.util.concurrent.{Executors, LinkedBlockingQueue}
 
+import org.bugogre.crawler.config.SecConfig
 import org.bugogre.crawler.html.Html
+import org.bugogre.crawler.httpclient.Web
 import org.bugogre.crawler.url.Url
 import org.jsoup.Jsoup
+
+import scala.concurrent.{Future, ExecutionContext}
+import scala.concurrent._
 
 /**
  * Created by xiachen on 12/16/14.
  */
-class HtmlParser {
+class HtmlParser extends Runnable{
+  val webs = new LinkedBlockingQueue[Web[Url]]()
   def parse(html: String): Html = {
     val doc = Jsoup.parse(html)
     Html(doc.title, doc, null)
@@ -17,4 +24,15 @@ class HtmlParser {
     val doc = Jsoup.parse(html)
     Html(doc.title, doc, url)
   }
+
+  def run(): Unit = {
+    while(true) {
+      val web = webs take
+      val html: Future[Html] = future {
+        parse(web.html)
+      }
+    }
+  }
+
+  implicit val ec = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(SecConfig.threads.getInt("fetcher")))
 }
