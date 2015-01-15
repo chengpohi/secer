f278506507b33e5386372f5f79debcc0d27c6609
xiachen <chengpohi@gmail.com>
2015-01-15 22:44:07 +0800

    mv CrawlerRunner to Crawler

--- src/main/scala/org/bugogre/crawler/app/Crawler.scala
+++ src/main/scala/org/bugogre/crawler/app/Crawler.scala
@@ -6,12 +6,33 @@ import org.bugogre.crawler.fetcher._
 import org.bugogre.crawler.url.Url
 import org.bugogre.crawler.parser.HtmlParser
 
+import akka.actor.Actor
+import akka.actor.Props
+
+import org.slf4j.LoggerFactory
 
 /**
  * author: chengpohi@gmail.com
  */
 object Crawler{
   def main(args: Array[String]) {
-    akka.Main.main(Array(classOf[CrawlerRunner].getName))
+    akka.Main.main(Array(classOf[Crawler].getName))
+  }
+}
+
+class Crawler extends Actor {
+
+  val LOG = LoggerFactory.getLogger(getClass.getName);
+
+  val webFetcher = context.actorOf(Props[WebFetcher], "webFetcher")
+
+  override def preStart(): Unit = {
+    webFetcher ! Url("http://www.baidu.com")
+  }
+
+  def receive = {
+    case str: String => {
+      println(str)
+    }
   }
 }
