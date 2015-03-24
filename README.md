4c4413abe0328d07112643790554609bae73d28c
xiachen <chengpohi@gmail.com>
2015-03-24 22:46:39 +0800

    add build status

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/chengpohi/secer.svg)](https://travis-ci.org/chengpohi/secer)
+
 seccrawler
 ======
---
+++ src/main/scala/org/bugogre/crawler/Test.java
@@ -0,0 +1,14 @@
+package org.bugogre.crawler;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by xiachen on 3/22/15.
+ */
+public class Test {
+    public static void main(String[] args) {
+    }
+}
+
