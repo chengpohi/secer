4746518fcf8ca57bfb47f8b62ef77ebe1410d194
Xiang Chen <chengpohi@gmail.com>
2016-10-11 20:31:42 +0800

    DONE:repl

--- app/src/main/scala/com/github/chengpohi/app/Crawler.scala
+++ app/src/main/scala/com/github/chengpohi/app/Crawler.scala
@@ -1,10 +1,11 @@
 package com.github.chengpohi.app
 
 import akka.actor._
-import com.github.chengpohi.PageFetcherService
+import com.github.chengpohi.{ELKRunEngine, PageFetcherService}
 import com.github.chengpohi.app.http.HttpRunner
 import com.github.chengpohi.app.http.actions.RestActions.registerHandler
-import com.github.chengpohi.model.FetchItem
+import com.github.chengpohi.model.{DSL, FetchItem}
+import com.github.chengpohi.registry.ELKCommandRegistry
 import com.typesafe.config.ConfigFactory
 import org.jboss.netty.handler.codec.http.HttpMethod._
 import org.slf4j.LoggerFactory
@@ -25,6 +26,7 @@ class Crawler extends Actor with HttpRunner {
   override lazy val LOG = LoggerFactory.getLogger(getClass.getName)
   override lazy val config = ConfigFactory.load("http")
   lazy val fetcher = context.actorOf(Props[PageFetcherService], "fetcher")
+  lazy val replInterpreter = new ELKRunEngine(ELKCommandRegistry)
 
   override def preStart(): Unit = this.start()
 
@@ -42,7 +44,10 @@ class Crawler extends Actor with HttpRunner {
     s"""{"indexName": ${fetchItem.indexName}}"""
   }
 
+  def repl(dsl: DSL): String = replInterpreter.run(dsl.dsl)
+
   override def registerPath(): Unit = {
     registerHandler(POST, "/seed", seed)
+    registerHandler(POST, "/repl", repl)
   }
 }
--- indexer/src/main/scala/com/github/chengpohi/model/FetchItem.scala
+++ indexer/src/main/scala/com/github/chengpohi/model/FetchItem.scala
@@ -5,3 +5,4 @@ package com.github.chengpohi.model
  * Created by chengpohi on 3/18/16.
  */
 case class FetchItem(url: String, indexName: String, indexType: String, selectors: List[FieldSelector], urlRegex: Option[String] = None)
+case class DSL(dsl: String)
