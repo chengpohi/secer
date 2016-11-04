82bba59e17ea1681aeb557ca30e5ba1e3ce20b92
Xiang Chen <chengpohi@gmail.com>
2016-11-04 22:44:47 +0800

    DONE:app image

--- as/src/main/scala/com/github/chengpohi/scheduler/FeedScheduler.scala
+++ as/src/main/scala/com/github/chengpohi/scheduler/FeedScheduler.scala
@@ -16,18 +16,20 @@ import org.slf4j.LoggerFactory
   * Created by chengpohi on 10/13/16.
   */
 class FeedScheduler(feeds: List[Feed], crawler: ActorRef) extends Runnable {
-  lazy val LOGGER = LoggerFactory.getLogger(getClass.getName)
+  lazy val logger = LoggerFactory.getLogger(getClass.getName)
   implicit val formats = org.json4s.DefaultFormats
 
   override def run(): Unit = {
     val fetchItems: List[FetchItem] = feeds.flatMap(f => {
-      LOGGER.info("get feed type: {}, genre: {}, url: {}", f.feedType, f.genre, f.url)
-      val body: String = Jsoup.connect(f.url).execute().body()
+      logger.info("get feed type: {}, genre: {}, url: {}", f.feedType, f.genre, f.url)
+      val body: String = Jsoup.connect(f.url).timeout(1000 * 20).execute().body()
       for {
         r <- (parse(body) \ "feed" \\ "link" \\ "href").children.map(i => i.extract[String])
       } yield FetchItem(r, "appstore", f.genre, SELECTORS, Some("^https:\\/\\/itunes\\.apple\\.com\\/%s\\/app\\/.*".format(f.country)))
     })
-    crawler ! "Hello Foo Bar"
-    println(fetchItems.size)
+    fetchItems.foreach(item => {
+      crawler ! item
+    })
+    logger.info("send {} items finished", fetchItems.size)
   }
 }
