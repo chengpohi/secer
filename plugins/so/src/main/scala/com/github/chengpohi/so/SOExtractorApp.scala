68d0f4e7b9bdcd1d81081792a8c5eeb707badf60
Xiang Chen <chengpohi@gmail.com>
2017-01-04 00:42:33 +0800

    DONE:filter function and answers indexer

--- plugins/so/src/main/scala/com/github/chengpohi/so/SOExtractorApp.scala
+++ plugins/so/src/main/scala/com/github/chengpohi/so/SOExtractorApp.scala
@@ -6,6 +6,8 @@ import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
 import com.github.chengpohi.indexer.{Finished, PageIndexerService}
 import com.typesafe.config.ConfigFactory
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by xiachen on 30/12/2016.
   */
@@ -27,16 +29,35 @@ object SOExtractorApp {
     //val file = new File("/Users/xiachen/IdeaProjects/secer/plugins/so/src/test/resources/so.xml")
     val filterTag = "java"
 
+    var ids = new ArrayBuffer[Int]()
+
     val f = (s: String) => s.contains(filterTag) && !s.contains("ParentId=")
 
-    val posts = SOExtractor().extract(file)(f)
     val actorSystem = ActorSystem("Crawler", ConfigFactory.load("crawler"))
     val soExtractor = actorSystem.actorOf(Props(new SOExtractorApp()))
-    posts.foreach(post => {
-      post.setIndexType(filterTag)
-      soExtractor ! post
-    })
-    println("Index Finished!")
+    val posts = SOExtractor().extract(file)(f)
+    posts.filter(_.tags.contains(filterTag))
+      .foreach(post => {
+        post.setIndexType(filterTag)
+        ids.append(post.Id)
+        soExtractor ! post
+      })
+    println("Index Questions Finished!")
+    println("-" * 10)
+    println("Index Answers!")
+
+    val f2 = (s: String) => s.contains("ParentId=") && ids.exists(id => s.contains(id.toString))
+    val answers = SOExtractor().extract(file)(f2)
+    answers.filter(_.parentId.isDefined)
+      .filter(a => ids.contains(a.parentId.get))
+      .foreach(answer => {
+        answer.setIndexType("answer")
+        soExtractor ! answer
+      })
+
+    println("Index Answers Finished!")
+
     actorSystem.terminate()
   }
 }
+
--- plugins/so/src/test/scala/com/github/chengpohi/so/SOExtractorTest.scala
+++ plugins/so/src/test/scala/com/github/chengpohi/so/SOExtractorTest.scala
@@ -9,19 +9,27 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class SOExtractorTest extends FlatSpec with Matchers {
   it should "parse row to post model" in {
+
+    val f = (s: String) => s.contains("opacity") && !s.contains("ParentId=")
     val file = new File(getClass.getResource("/so.xml").toURI)
     //val file = new File("/Users/xiachen/IdeaProjects/data/Posts.xml")
-    val posts = SOExtractor().extract(file)
+    val posts = SOExtractor().extract(file)(f)
     //println(posts.size)
     val head = posts.next()
     head.Id should be(4)
     head.tags.size should be(5)
-    head.doc.size should be(17)
+    head.doc.size should be(18)
     println("url = " + head.url)
     println("id = " + head.Id)
     println("title = " + head.title)
     println("body = " + head.body)
     println("tags = " + head.tags)
     println("fields = " + head.doc)
+
+    val ids = Array(2)
+    val f2 = (s: String) => s.contains("ParentId=") && ids.exists(id => s.contains(id.toString))
+    val answers = SOExtractor().extract(file)(f2)
+    val answer = answers.next()
+    println(answer.id)
   }
 }
